ed8ece52865c95737d8ee6e109784f63eb6d3e5f
emerdenny <e@ewd.dev>
2026-03-10 03:01:11 -0400

    model structure shell

--- main.py
+++ main.py
@@ -18,6 +18,25 @@ class GPT(nn.Module):
         super().__init__()
         self.config = config
 
+        # GPT2 is a decoder-only model, no encoder w/ cross attn like in AIAYN
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(
+                    config.vocab_size, config.n_embd
+                ),  # weights of token embeddings
+                wpe=nn.Embedding(
+                    config.block_size, config.n_embd
+                ),  # weights of position embeddings
+                h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+                ln_f=nn.LayerNorm(
+                    config.n_embd
+                ),  # from GPT2 paper, not shown in AIAYN transformer figure
+            )
+        )
+        self.lm_head = nn.Linear(
+            config.n_embd, config.vocab_size, bias=False
+        )  # projects from embedding dim to token dim for output
+
 
 def main():
     print("Hello from reproduce-gpt2!")
